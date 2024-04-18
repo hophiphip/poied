c74a20085b757aa2eecd12e0a49e4cfb0dae71ec
hophiphip <nighop@yandex.ru>
2024-04-18 11:36:42 +0400

    Remove strict eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
   },
   extends: [
       'eslint:recommended', 
-      'plugin:@typescript-eslint/strict-type-checked', 
       'plugin:@typescript-eslint/stylistic-type-checked', 
       'plugin:react/recommended', 
       'plugin:react/jsx-runtime', 
